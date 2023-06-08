2f283aa7c48198b7702d9f8e838112086809df73
Dimitar Vlahov <dimitarvlahov24@gmail.com>
2023-06-08 14:08:37 +0300

    .

--- controllers/devCryptoController.js
+++ controllers/devCryptoController.js
@@ -3,19 +3,29 @@ const devCryptoController = require('express').Router();
 const cryptoApi = require('../api/cryptoApi');
 const errorParser = require('../utils/errorParser');
 
-const { dummyTopThree, dummyTopHundred, dummyNewCoins, dummyCryptoMap, dummyFiatMap, dummyAllCoins, dummyGlobalData, dummyCoinDetails, dummyCoinOHLC } = require('../dummyData');
+const {
+  dummyTopThree,
+  dummyTopHundred,
+  dummyNewCoins,
+  dummyCryptoMap,
+  dummyFiatMap,
+  dummyAllCoins,
+  dummyGlobalData,
+  dummyCoinDetails,
+  dummyCoinOHLC,
+} = require('../dummyData');
 
 devCryptoController.get('/topThree', async (req, res) => {
   try {
     res.status(200).json(dummyTopThree);
   } catch (error) {
     let statusCode = 400;
-    if (error.response.status) {
+    if (error.response?.status) {
       statusCode = error.response.status;
     }
 
     res.status(statusCode).json({
-      message: errorParser(error)
+      message: errorParser(error),
     });
   }
 });
@@ -27,12 +37,12 @@ devCryptoController.get('/topHundred', async (req, res) => {
     res.status(200).json(dummyTopHundred);
   } catch (error) {
     let statusCode = 400;
-    if (error.response.status) {
+    if (error.response?.status) {
       statusCode = error.response.status;
     }
 
     res.status(statusCode).json({
-      message: errorParser(error)
+      message: errorParser(error),
     });
   }
 });
@@ -44,12 +54,12 @@ devCryptoController.get('/newCoins', async (req, res) => {
     res.status(200).json(dummyNewCoins);
   } catch (error) {
     let statusCode = 400;
-    if (error.response.status) {
+    if (error.response?.status) {
       statusCode = error.response.status;
     }
 
     res.status(statusCode).json({
-      message: errorParser(error)
+      message: errorParser(error),
     });
   }
 });
@@ -59,12 +69,12 @@ devCryptoController.get('/cryptoMap', async (req, res) => {
     res.status(200).json(dummyCryptoMap);
   } catch (error) {
     let statusCode = 400;
-    if (error.response.status) {
+    if (error.response?.status) {
       statusCode = error.response.status;
     }
 
     res.status(statusCode).json({
-      message: errorParser(error)
+      message: errorParser(error),
     });
   }
 });
@@ -74,12 +84,12 @@ devCryptoController.get('/fiatMap', async (req, res) => {
     res.status(200).json(dummyFiatMap);
   } catch (error) {
     let statusCode = 400;
-    if (error.response.status) {
+    if (error.response?.status) {
       statusCode = error.response.status;
     }
 
     res.status(statusCode).json({
-      message: errorParser(error)
+      message: errorParser(error),
     });
   }
 });
@@ -89,28 +99,32 @@ devCryptoController.get('/allCoins', async (req, res) => {
     res.status(200).json(dummyAllCoins);
   } catch (error) {
     let statusCode = 400;
-    if (error.response.status) {
+    if (error.response?.status) {
       statusCode = error.response.status;
     }
 
     res.status(statusCode).json({
-      message: errorParser(error)
+      message: errorParser(error),
     });
   }
 });
 
 devCryptoController.get('/convert', async (req, res) => {
   try {
-    const response = await cryptoApi.convert(req.query.amount, req.query.from, req.query.to);
+    const response = await cryptoApi.convert(
+      req.query.amount,
+      req.query.from,
+      req.query.to
+    );
     res.status(200).json(response);
   } catch (error) {
     let statusCode = 400;
-    if (error.response.status) {
+    if (error.response?.status) {
       statusCode = error.response.status;
     }
 
     res.status(statusCode).json({
-      message: errorParser(error)
+      message: errorParser(error),
     });
   }
 });
@@ -120,12 +134,12 @@ devCryptoController.get('/getGlobalData', async (req, res) => {
     res.status(200).json(dummyGlobalData);
   } catch (error) {
     let statusCode = 400;
-    if (error.response.status) {
+    if (error.response?.status) {
       statusCode = error.response.status;
     }
 
     res.status(statusCode).json({
-      message: errorParser(error)
+      message: errorParser(error),
     });
   }
 });
@@ -135,29 +149,31 @@ devCryptoController.get('/getCoinDetails', async (req, res) => {
     res.status(200).json(dummyCoinDetails);
   } catch (error) {
     let statusCode = 400;
-    if (error.response.status) {
+    if (error.response?.status) {
       statusCode = error.response.status;
     }
 
     res.status(statusCode).json({
-      message: errorParser(error)
+      message: errorParser(error),
     });
   }
 });
 
-
 devCryptoController.get('/getCoinChartData', async (req, res) => {
   try {
-    const response = await cryptoApi.getCoinChartData(req.query.coinId, req.query.days);
+    const response = await cryptoApi.getCoinChartData(
+      req.query.coinId,
+      req.query.days
+    );
     res.status(200).json({ chartData: response });
   } catch (error) {
     let statusCode = 400;
-    if (error.response.status) {
+    if (error.response?.status) {
       statusCode = error.response.status;
     }
 
     res.status(statusCode).json({
-      message: errorParser(error)
+      message: errorParser(error),
     });
   }
 });
@@ -172,7 +188,7 @@ devCryptoController.get('/getCoinOHLC', async (req, res) => {
     }
 
     res.status(statusCode).json({
-      message: errorParser(error)
+      message: errorParser(error),
     });
   }
 });
@@ -183,12 +199,12 @@ devCryptoController.get('/search', async (req, res) => {
     res.status(200).json(response);
   } catch (error) {
     let statusCode = 400;
-    if (error.response.status) {
+    if (error.response?.status) {
       statusCode = error.response.status;
     }
 
     res.status(statusCode).json({
-      message: errorParser(error)
+      message: errorParser(error),
     });
   }
 });
