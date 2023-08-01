9fde0596904892c41cddc6aa99392fe0a8d3ce69
Dimitar Vlahov <dimitarvlahov24@gmail.com>
2023-08-01 14:47:29 +0300

    Changes to CORS

--- index.js
+++ index.js
@@ -1,21 +1,23 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
+const corsConfig = require('./configs/corsConfig');
 const databaseConfig = require('./configs/database');
 
 const tokenParser = require('./middlewares/tokenParser');
 const { isAuthenticated } = require('./middlewares/guards');
-const cors = require('./middlewares/cors');
-const EXPRESS_PORT = process.env.EXPRESS_PORT;
 
 const cryptoController = require('./controllers/cryptoController');
 const devCryptoController = require('./controllers/devCryptoController');
 const portfolioController = require('./controllers/portfolioController');
 
+const EXPRESS_PORT = process.env.EXPRESS_PORT;
+
 async function start() {
 	const app = express();
 	const connectToDB = databaseConfig();
 
-	app.use(cors());
+	app.use(cors(corsConfig));
 	app.use(express.json());
 	app.use(tokenParser());
 
